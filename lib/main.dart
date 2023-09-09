f982fb7ab08df380b34243180e3d39980b5f2614
kiruthika <kiruthika210504@gmail.com>
2023-09-09 20:42:43 +0530

    added const modifier to the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'board.dart';
 
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home:GameBoard(),
 
